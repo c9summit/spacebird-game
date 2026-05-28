29419b24c4c049ec37137a2d6ed03c044107cd42
Vrishank05 <vpat505@aucklanduni.ac.nz>
2026-05-28 23:13:53 +1200

    Changed stars shapes

--- bg_renderer.vhd
+++ bg_renderer.vhd
@@ -4,70 +4,51 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity bg_renderer is
     port (
-        clk          : in  std_logic;
-        pixel_row    : in  std_logic_vector(9 downto 0);
-        pixel_column : in  std_logic_vector(9 downto 0);
-        scroll_en    : in  std_logic;
-        red, green, blue : out std_logic_vector(3 downto 0)
-
-    );
+        clk : in std_logic;
+        pixel_row, pixel_column : in std_logic_vector(9 downto 0);
+        scroll_en: in std_logic;
+        red, green, blue : out std_logic_vector(3 downto 0));
 end bg_renderer;
 
 architecture behaviour of bg_renderer is
-
     signal scroll_x : unsigned(9 downto 0) := (others => '0');
-
 begin
-
     process(clk)
     begin
-
         if rising_edge(clk) then
             if scroll_en = '1' then
                 scroll_x <= scroll_x + 1;
             end if;
-
         end if;
-
     end process;
 
     process(pixel_row, pixel_column, scroll_x)
 
-    variable x  : integer;
-    variable y  : integer;
+    variable x : integer;
+    variable y : integer;
     variable sx : integer;
 
 begin
-
-    x  := to_integer(unsigned(pixel_column));
-    y  := to_integer(unsigned(pixel_row));
+    x := to_integer(unsigned(pixel_column));
+    y := to_integer(unsigned(pixel_row));
     sx := to_integer(scroll_x);
 
-    -- Black background
-    red   <= "0000";
+    red <= "0000";
     green <= "0000";
-    blue  <= "0000";
+    blue <= "0000";
 
-    -- White stars (4x4 pixels)
-    if (((x + sx) mod 63 < 4) and
-        (y mod 47 < 4)) then
-
-        red   <= "1111";
+    -- White stars
+    if (((x + sx) mod 63 < 4) and ((y + sx) mod 47 < 4)) then
+        red <= "1111";
         green <= "1111";
-        blue  <= "1111";
-
+        blue <= "1111";
     end if;
 
-    -- Yellow stars (3x3 pixels)
-    if (((x + (sx/2)) mod 97 < 3) and
-        (y mod 83 < 3)) then
-
-        red   <= "1111";
-        green <= "1010";
-        blue  <= "0010";
-
+    -- Yellow stars
+    if (((x + (sx/2)) mod 97 < 3) and (y mod 83 < 3)) then
+        red <= "1111";
+        green <= "1111";
+        blue <= "0000";
     end if;
-
 end process;
-
 end behaviour;
